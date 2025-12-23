2f9870e7a06a35eb1bb7fc8e8b2bc3a91f264b79
Ishanphogat17 <Ishanphogat17@gmail.com>
2025-12-23 10:27:15 +0530

    worked on arg handling

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from stats import count_letters
 from stats import sort_letter_counts
 import sys 
 
+ 
+
 def get_book_text(path_to_file):
     with open(path_to_file, encoding='utf-8') as f:
         file_contents = f.read()
@@ -10,11 +12,14 @@ def get_book_text(path_to_file):
 
 
 def main():
-    path_to_file = sys.argv[1]
-    text = get_book_text(path_to_file)
-    total_words = total_words_in_text(text)
-    letter_counts = count_letters(text)
-    sorted_letters = sort_letter_counts(letter_counts)
+    if len(sys.argv) < 2: 
+        sys.exit(1)
+    else:    
+        path_to_file = sys.argv[1]
+        text = get_book_text(path_to_file)
+        total_words = total_words_in_text(text)
+        letter_counts = count_letters(text)
+        sorted_letters = sort_letter_counts(letter_counts)
     
     print(f"Found {total_words} total words")
     print("\nLetter counts (sorted from most to least frequent):")
